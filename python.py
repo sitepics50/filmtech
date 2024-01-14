4a44da1421250366608548ea14e27d9c0f4107e2
follow95 <129000450+follow95@users.noreply.github.com>
2024-01-14 19:33:05 +0100

    Delete python.py

--- python.py
+++
@@ -1,7 +0,0 @@
-from rembg import remove
-from PIL import Image
-input_path = 'icl.jpg'
-output_path = 'facebook.jpg'
-input = Image.open(input_path)
-output = remove(input)
-output.save(output.path)
